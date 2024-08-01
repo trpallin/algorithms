f6b209a22b43101bf253549229cfef2d20ed9574
sicd <julonfiv@gmail.com>
2024-08-01 14:36:38 -0700

    solved

---
+++ leetcode/80.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return len(nums)
+        insert_pos = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[insert_pos - 2]:
+                nums[insert_pos] = nums[i]
+                insert_pos += 1
+        return insert_pos
