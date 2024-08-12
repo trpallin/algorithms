f3f36679e589bc06922576756c8555182fb45cbd
sicd <julonfiv@gmail.com>
2024-08-12 05:30:07 -0700

    solved

---
+++ leetcode/98.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean isValidBST(TreeNode root, Integer lower, Integer upper) {
+        if (root == null) {
+            return true;
+        }
+        if (lower != null && root.val <= lower) {
+            return false;
+        }
+        if (upper != null && root.val >= upper) {
+            return false;
+        }
+        if (!isValidBST(root.left, lower, root.val) || !isValidBST(root.right, root.val, upper)) {
+            return false;
+        }
+        return true;
+    }
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+}
